5ba7e5911ddd6eb5d6c00a40dd3b8fe9efa1deaf
IsaqueCruzDev <oliveiraioc616@gmail.com>
2023-07-12 17:35:16 -0300

    json vercel

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
 
+const port = process.env.PORT zz 3001;
+
 const app = express();
 app.use(cors());
 app.use(express.json());
